ea2c848cb2f11d6ca1add06d3706fa3b5e587465
InscrutableTaco <jonathan.w.d.gibson@gmail.com>
2025-03-25 01:06:47 -0700

    player movement

--- main.py
+++ main.py
@@ -3,8 +3,7 @@
 # throughout this file
 import pygame
 from constants import *
-
-
+from player import *
 
 def main():
     pygame.init
@@ -12,19 +11,29 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
-
+    print_debug = True
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
+        #print(player)  # This should print something like <Player object at 0x...>
         screen.fill(000000, rect=None, special_flags=0)
+
+        player.update(dt)
+        player.draw(screen)
+
+        if print_debug == True:
+            print(f"player position = {player.position}")
+            print_debug = False
+
         pygame.display.flip()
         clock.tick(60)
         dt = clock.tick(60)/1000
+
 
 
 if __name__ == "__main__":
