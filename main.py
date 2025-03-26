de8f91f78b68a614b70274b933794eddd193df6b
InscrutableTaco <jonathan.w.d.gibson@gmail.com>
2025-03-25 17:31:09 -0700

    updated groups

--- main.py
+++ main.py
@@ -5,26 +5,40 @@ import pygame
 from constants import *
 from player import *
 
+
+
 def main():
     pygame.init
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
+
+    drawable = pygame.sprite.Group()
+    updatable = pygame.sprite.Group()
+
+    Player.containers = (drawable, updatable)
+
+
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
     dt = 0
+
+
+
     print_debug = True
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        #print(player)  # This should print something like <Player object at 0x...>
+        print(player)  # This should print something like <Player object at 0x...>
         screen.fill(000000, rect=None, special_flags=0)
 
-        player.update(dt)
-        player.draw(screen)
+
+        updatable.update(dt)
+        for entity in drawable:
+            entity.draw(screen)
 
         if print_debug == True:
             print(f"player position = {player.position}")
